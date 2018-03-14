c10c39560618aaacd90e37135c1fcec6435300b8
liruiyu970105 <1055469011@qq.com>
2018-03-14 13:32:14 +0800

    v1.4

--- src/main/java/com/_51job/dao/MyDao.java
+++ src/main/java/com/_51job/dao/MyDao.java
@@ -11,75 +11,75 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class MyDao {
+public abstract class MyDao {
 
-	private final SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
 
-	@Autowired
-	public MyDao(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
+    @Autowired
+    public MyDao(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
-	private Session getSession(){
-		return sessionFactory.openSession();
-	}
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
 
-	public void afterTransaction(Session session,Transaction transaction){
-		transaction.commit();
-		session.close();
-	}
+    public void afterTransaction(Session session,Transaction transaction){
+        transaction.commit();
+        session.close();
+    }
 
-	public int save(Object object){
-		Session session=getSession();
-		Transaction transaction=session.beginTransaction();
-		int id=(Integer)session.save(object);
-		afterTransaction(session,transaction);
-		return id;
-	}
+    public int save(Object object){
+        Session session=getSession();
+        Transaction transaction=session.beginTransaction();
+        int id=(Integer)session.save(object);
+        afterTransaction(session,transaction);
+        return id;
+    }
 
     /**
      * 批存储
      */
-	public List<Integer> batchSave(List<Object> list){
-	    List<Integer> ids=new ArrayList<>();
-	    Session session=getSession();
-	    Transaction transaction=session.beginTransaction();
-	    for (Object object: list){
-	        ids.add((Integer) session.save(object));
+    public List<Integer> batchSave(List<Object> list){
+        List<Integer> ids=new ArrayList<>();
+        Session session=getSession();
+        Transaction transaction=session.beginTransaction();
+        for (Object object: list){
+            ids.add((Integer) session.save(object));
         }
         afterTransaction(session,transaction);
         return ids;
     }
 
-	public <T> T get(Class<T> tClass, int id){
-		return getSession().get(tClass,id);
-	}
+    public <T> T get(Class<T> tClass, int id){
+        return getSession().get(tClass,id);
+    }
 
-	public <T> List<T> getPart(Class<T> tClass, int start, int end){
-		Query<T> query=getSession().createQuery("from "+tClass.getSimpleName(),tClass);
-		query.setFirstResult(start);
-		query.setMaxResults(end-start);
-		return query.list();
-	}
+    public <T> List<T> getPart(Class<T> tClass, int start, int end){
+        Query<T> query=getSession().createQuery("from "+tClass.getSimpleName(),tClass);
+        query.setFirstResult(start);
+        query.setMaxResults(end-start);
+        return query.list();
+    }
 
-	public void delete(Object object){
-		Session session=getSession();
-		Transaction transaction=session.beginTransaction();
-		session.delete(object);
+    public void delete(Object object){
+        Session session=getSession();
+        Transaction transaction=session.beginTransaction();
+        session.delete(object);
         afterTransaction(session,transaction);
-	}
+    }
 
-	public void update(Object object){
-		Session session=getSession();
-		Transaction transaction=session.beginTransaction();
-		session.saveOrUpdate(object);
+    public void update(Object object){
+        Session session=getSession();
+        Transaction transaction=session.beginTransaction();
+        session.saveOrUpdate(object);
         afterTransaction(session,transaction);
-	}
+    }
 
-	public <T> List<T> allObjects(Class<T> tClass){
-		Query<T> query=getSession().createQuery("from "+tClass.getName()+" order by id asc ",tClass);
-		return query.list();
-	}
+    public <T> List<T> allObjects(Class<T> tClass){
+        Query<T> query=getSession().createQuery("from "+tClass.getName()+" order by id asc ",tClass);
+        return query.list();
+    }
 
     /**
      * 一对多查询
@@ -87,9 +87,9 @@ public class MyDao {
      * @param oneClass 单个的class
      * @param manyClass 多个的class
      */
-	public <T> List<T> oneToMany(int id_one, Class oneClass, Class<T> manyClass){
-	    Query<T> query=getSession().createQuery("from "+manyClass.getSimpleName()+" where "+oneClass.getSimpleName()+"_id="+id_one,manyClass);
-	    return query.list();
+    public <T> List<T> oneToMany(int id_one, Class oneClass, Class<T> manyClass){
+        Query<T> query=getSession().createQuery("from "+manyClass.getSimpleName()+" where "+oneClass.getSimpleName()+"_id="+id_one,manyClass);
+        return query.list();
     }
 
 }
--- src/main/java/com/_51job/web/ApplicantController.java
+++ src/main/java/com/_51job/web/ApplicantController.java
@@ -1,28 +1,62 @@
 package com._51job.web;
 
+import com._51job.domain.User;
+import com._51job.service.ApplicantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 @RequestMapping(value = "/applicant")
 public class ApplicantController {
+
+    @Autowired
+    private ApplicantService applicantService;
+    //OK
+    //保存/修改简历
     @RequestMapping(value = "/saveResume", method = RequestMethod.POST)
     @ResponseBody
-    public boolean saveResume(){
+    public boolean saveResume(int user_id,String str_applicant,String str_language,
+                              String str_skill, String str_workexp,
+                              String str_eduexp, String prelocation,
+                              String preindustry, String prefunction){
+
+        int save_size = applicantService.saveResume(user_id,str_applicant, str_language,str_skill,str_workexp,
+                str_eduexp,prelocation,preindustry,prefunction);
+        if(save_size ==8){return true;}
         return false;
     }
-
+    //OK
     // 获取匹配的岗位
-    @RequestMapping(value = "suitableJobs", method = RequestMethod.GET)
+    @RequestMapping(value = "/suitableJobs", method = RequestMethod.GET)
     @ResponseBody
-    public void suitableJobs(int start, int end){
-        return;
+    public List<EnterpriseResume> suitableJobs(int start, int end) {
+        if (end <= start) return null;
+        else {
+            return applicantService.suitJobs(start,end);
+        }
     }
-
+    //OK
     //获取投递了的岗位以及对应的状态
-    @RequestMapping(value = "sentJobs")
+    @RequestMapping(value = "/sentJobs")
     @ResponseBody
-    public void appliedJobs(int start, int end){
-        return;
+    public List<PostInfoState> appliedJobs(int user_id){
+        return applicantService.sentJobstate(user_id);
+    }
+    //OK
+    //注册
+    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    public boolean register(String account, String password, HttpServletRequest request){
+
+        User applicant = applicantService.register(account,password);
+        if(applicant!=null){
+            request.getSession().setAttribute("applicant",applicant);
+            return true;
+        }
+        else return false;
+
     }
 }
