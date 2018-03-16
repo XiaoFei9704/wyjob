1883fb7ae210277bc1e84e67773000433c4b3012
jdfohewk <13319202082@163.com>
2018-03-16 11:19:17 +0800

    搜索完成

--- src/main/java/com/_51job/tool/DataUtil.java
+++ src/main/java/com/_51job/tool/DataUtil.java
@@ -14,14 +14,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-public class DataUtil implements Runnable{
+public class  DataUtil implements Runnable{
     private static SessionFactory sessionFactory;
     private static Jedis jedis;
+    private static List<Recruitment> recruitments;
+    private static List<Enterprise> enterprises;
 
     static {
         jedis = new Jedis("localhost");
         Configuration configuration = new Configuration().configure();
         sessionFactory= configuration.buildSessionFactory();
+        recruitments=new ArrayList<>();
+        enterprises=new ArrayList<>();
         new Thread(new DataUtil()).start();
     }
 
@@ -37,21 +41,31 @@ public class DataUtil implements Runnable{
     }
 
     public static List<Recruitment> allRecruitments(){
-        Set<byte[]> keys=jedis.keys("rec*".getBytes());
-        List<Recruitment> recruitments=new ArrayList<>();
-        for(byte[] key: keys){
-            recruitments.add((Recruitment) SerializeUtil.unserialize(jedis.get(key)));
-        }
         return recruitments;
     }
 
     public static List<Enterprise> allEnterprises(){
-        Set<byte[]> keys=jedis.keys("ent*".getBytes());
-        List<Enterprise> enterprises=new ArrayList<>();
-        for(byte[] key: keys){
-            enterprises.add((Enterprise) SerializeUtil.unserialize(jedis.get(key)));
-        }
         return enterprises;
+    }
+    //根据一个enterpriseId在redis里搜索到该公司的所有招聘信息
+    public static List<Recruitment> recruitmentsOfAnEnterprise(int enterpriseId){
+        List<Recruitment> recruitments=new ArrayList<>();
+        Set<byte[]> keys=jedis.keys("rec*".getBytes());
+        for(byte[] key: keys){
+            Recruitment recruitment=(Recruitment) SerializeUtil.unserialize(jedis.get(key));
+            if(recruitment.getEnterpriseId().equals(enterpriseId))recruitments.add(recruitment);
+        }
+        return recruitments;
+    }
+    //根据一个enterpriseId在redis里搜索到该公司的信息
+    public static Enterprise getEnterprise(int enterpriseId){
+        byte[] key=String.valueOf("ent"+enterpriseId).getBytes();
+        return (Enterprise) SerializeUtil.unserialize(jedis.get(key));
+    }
+    //根据一个recruitmentId在redis里搜索到该公司的信息
+    public static Recruitment getRecruitment(int recruitmentId){
+        byte[]key=String.valueOf("rec"+recruitmentId).getBytes();
+        return (Recruitment) SerializeUtil.unserialize(jedis.get(key));
     }
 
     public static List<Dictionary> allDictionaries(){
@@ -145,17 +159,14 @@ public class DataUtil implements Runnable{
         return dictionaries;
     }
 
-
-
-
-    public static List<Recruitment> recruitmentsOfAnEnterprise(int enterpriseId){
-        List<Recruitment> recruitments=new ArrayList<>();
-        Set<byte[]> keys=jedis.keys("rec*".getBytes());
-        for(byte[] key: keys){
-            Recruitment recruitment=(Recruitment)SerializeUtil.unserialize(jedis.get(key));
-            if(recruitment.getEnterpriseId().equals(enterpriseId))recruitments.add(recruitment);
+    public static List<Enterprise> getEnterprisesByDomicile(int domicile){
+        Set<byte[]> enter_ids=jedis.smembers(("d_p"+domicile).getBytes());
+        List<Enterprise> enterprises=new ArrayList<>();
+        for(byte[] enterid: enter_ids){
+            int id=byteArrayToInt(enterid);
+            enterprises.add((Enterprise) SerializeUtil.unserialize(jedis.get(("ent"+id).getBytes())));
         }
-        return recruitments;
+        return enterprises;
     }
 
 
@@ -168,6 +179,7 @@ public class DataUtil implements Runnable{
             query.setMaxResults(count);
             for(Recruitment recruitment: query.list()){
                 jedis.set(("rec"+ recruitment.getRecruitmentId()).getBytes(), SerializeUtil.serialize(recruitment));
+                recruitments.add(recruitment);
             }
         }else {
             Query<Enterprise> query=session.createQuery("from Enterprise ",Enterprise.class);
@@ -175,6 +187,8 @@ public class DataUtil implements Runnable{
             query.setMaxResults(count);
             for(Enterprise enterprise: query.list()){
                 jedis.set(("ent"+enterprise.getEnterpriseId()).getBytes(),SerializeUtil.serialize(enterprise));
+                jedis.sadd(("d_p"+enterprise.getDomicile()).getBytes(),intToByteArray(enterprise.getEnterpriseId()));
+                enterprises.add(enterprise);
             }
         }
         session.close();
@@ -200,6 +214,33 @@ public class DataUtil implements Runnable{
             for(int i=0;i<ent_count/2000+1;i++){
                 query(2000*i,2000,2);
             }
+        }else{
+            System.out.println("预加载开始");
+            Set<byte[]> keys=jedis.keys("rec*".getBytes());
+            for(byte[] key: keys){
+                recruitments.add((Recruitment) SerializeUtil.unserialize(jedis.get(key)));
+            }
+            System.out.println("加载完招聘信息");
+            keys=jedis.keys("ent*".getBytes());
+            for(byte[] key: keys){
+                enterprises.add((Enterprise) SerializeUtil.unserialize(jedis.get(key)));
+            }
+            System.out.println("预加载完毕！");
         }
+    }
+
+    public static int byteArrayToInt(byte[] b) {
+        return   b[3] & 0xFF |
+                (b[2] & 0xFF) << 8 |
+                (b[1] & 0xFF) << 16 |
+                (b[0] & 0xFF) << 24;
+    }
+    public static byte[] intToByteArray(int a) {
+        return new byte[] {
+                (byte) ((a >> 24) & 0xFF),
+                (byte) ((a >> 16) & 0xFF),
+                (byte) ((a >> 8) & 0xFF),
+                (byte) (a & 0xFF)
+        };
     }
 }
